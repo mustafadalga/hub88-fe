f827bc92cb18e2c133294531d161dae9b8ba1bc5
Mustafa Dalga <mustafadalgaa@gmail.com>
2024-10-29 17:02:03 +0200

    Design reusable datatable component

---
+++ src/components/DataTable.tsx
@@ -0,0 +1,50 @@
+export interface Column<T> {
+    header: string;
+    accessor: keyof T;
+}
+
+interface DataTableProps<T> {
+    columns: Column<T>[];
+    data: T[];
+    noMatchText?: string
+}
+
+export default function DataTable<T>({ columns, data, noMatchText = "No matching results found" }: DataTableProps<T>) {
+    return (
+        <table className="grid w-full rounded-md sm:rounded-lg overflow-hidden text-left"
+               aria-label="Data table">
+            <thead className="grid grid-cols-[100px_1fr] text-[10px] lg:text-sm text-white uppercase bg-indigo-500">
+            {columns.map(column => (
+                <th key={String(column.accessor)}
+                    scope="col"
+                    className="px-6 py-4">
+                    {column.header}
+                </th>
+            ))}
+            </thead>
+            <tbody>
+
+            {data.length > 0 ? (
+                data.map((row, rowIndex) => (
+                    <tr key={rowIndex}
+                        className="grid grid-cols-[100px_1fr] bg-white border-b hover:bg-gray-50 text-[10px] lg:text-sm">
+                        {columns.map(column => (
+                            <td key={String(column.accessor)}
+                                className="px-6 py-3">
+                                {String(row[column.accessor])}
+                            </td>
+                        ))}
+                    </tr>
+                ))
+            ) : (
+                <tr className="grid bg-white border-b">
+                    <td colSpan={columns.length} className="px-6 py-10 text-center text-sm lg:text-base">
+                        {noMatchText}
+                    </td>
+                </tr>
+            )}
+
+            </tbody>
+        </table>
+    )
+}
