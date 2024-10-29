507b84ae9b804a744142ee2f84cfea6682bbc8b6
Mustafa Dalga <mustafadalgaa@gmail.com>
2024-10-29 18:44:59 +0200

    Update src folder alias

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
+import { fileURLToPath, URL } from "node:url";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,7 +10,7 @@ export default defineConfig({
         globals: true,
         setupFiles: './jest-dom.setup.ts',
         alias: {
-            '@': './src/',
+            "@": fileURLToPath(new URL("./src", import.meta.url)),
         },
         exclude: [
             '**/node_modules/**',
